01637a01188e6da82b7dbb6a475a1da807632bed
mohamadaltalli-mamahealth <mohammadaltalli@gmail.com>
2024-08-29 17:05:38 +0200

    cgb works for nn.linear

--- network/computational_graph_builder.py
+++ network/computational_graph_builder.py
@@ -6,27 +6,23 @@ import torch
 class ComputationalGrapBuilder:
     def __init__(self):
         self.jacobian_chain: List[torch.Tensor] = []
+        self.eye = None
 
     def construct_graph(self, result: torch.Tensor) -> List[torch.Tensor]:
+        self.eye = torch.eye(result.shape[0])
         for i in range(len(result)):
             recursion_depth = 0
             grad_fn = result[i].grad_fn
-            derivatives = result[i].grad_fn(torch.tensor(1.0))
-            if isinstance(derivatives, tuple):
-                for index, derivative in enumerate(derivatives):
-                    if derivative is None:
-                        continue
-                    if index > 1:
-                        raise Exception("More indices than expected")
+            derivative = self._pick_derivative(
+                result[i].grad_fn(torch.tensor(1.0)), result[i].grad_fn
+            )
 
-                    self._add_to_jacobian_chain(derivative, recursion_depth)
-                    self._traverse_graph(
-                        grad_fn, recursion_depth, curent_seed=derivative
-                    )
-            else:
-                self._add_to_jacobian_chain(derivatives, recursion_depth)
-                self._traverse_graph(grad_fn, recursion_depth, curent_seed=derivatives)
+            self._add_to_jacobian_chain(derivative, recursion_depth)
+            self._traverse_graph(grad_fn, recursion_depth, curent_seed=derivative)
 
+        print(len(self.jacobian_chain))
+        print(self.jacobian_chain[9][9])
+        print(self.jacobian_chain[98][10])
         return self.jacobian_chain
 
     def _traverse_graph(
@@ -38,32 +34,40 @@ class ComputationalGrapBuilder:
         recursion_depth += 1
         next_functions: torch.Node = function.next_functions
         for _, function in enumerate(next_functions):
-            if function[0] is None:
+            if (
+                function[0] is None
+                or "AccumulateGrad" in function[0].__class__.__name__
+                or "TBackward" in function[0].__class__.__name__
+            ):
                 continue
-            derivatives = function[0](curent_seed)
-            if isinstance(derivatives, tuple):
-                for index, derivative in enumerate(derivatives):
-                    if derivative is None:
-                        continue
-                    if index > 1:
-                        raise Exception("More indices than expected")
 
-                    self._add_to_jacobian_chain(derivative, recursion_depth)
-                    self._traverse_graph(function[0], recursion_depth, curent_seed)
-
-            else:
-                self._add_to_jacobian_chain(derivatives, recursion_depth)
-                self._traverse_graph(function[0], recursion_depth, curent_seed)
+            derivative = self._pick_derivative(function[0](curent_seed), function[0])
+            self._add_to_jacobian_chain(derivative, recursion_depth)
+            self._traverse_graph(function[0], recursion_depth, curent_seed)
 
     def _add_to_jacobian_chain(
         self,
         derivative: torch.Tensor,
         index: int,
     ):
-        derivative_row = derivative.unsqueeze(0)
+        if derivative.shape[0] == 1:
+            derivative_row = derivative
+        else:
+            derivative_row = derivative.unsqueeze(0)
         if len(self.jacobian_chain) <= index:
             self.jacobian_chain.append(derivative_row)
         else:
             self.jacobian_chain[index] = torch.cat(
                 (self.jacobian_chain[index], derivative_row), dim=0
             )
+
+    def _pick_derivative(self, derivatives, function):
+        if isinstance(derivatives, tuple):
+            if "AddmmBackward" in function.__class__.__name__:
+                return derivatives[1]
+            if "MvBackward" in function.__class__.__name__:
+                return derivatives[1]
+            if "ViewBackward" in function.__class__.__name__:
+                return derivatives[1]
+        else:
+            return derivatives
--- network/playground.py
+++ network/playground.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+from computational_graph_builder import ComputationalGrapBuilder
 
 # a = torch.randn(2, requires_grad=True)
 # b = torch.tensor([[8.0, 0.0], [0.0, 2.0]])
@@ -60,85 +62,182 @@ import torch
 # print(dx[0] / dw[0])
 
 
-x = torch.tensor([8.0, 8.0], requires_grad=True)
-y = torch.tensor([[4.0, 4.0], [4.0, 4.0]])
+# x = torch.tensor([8.0, 8.0], requires_grad=True)
+# y = torch.tensor([[4.0, 4.0], [4.0, 4.0]])
 
-w = torch.matmul(y, x)
-v = torch.matmul(y, w)
-p = v**2
-l = torch.matmul(y, p)
+# w = nn.Linear(2, 2)(x)
+# v = nn.Linear(2, 2)(w)
+# p = v**2
+# l = nn.Linear(2, 2)(p)
 
-num_operations = 5
-counter = 0
-matrix_chain = []
+# cgb = ComputationalGrapBuilder()
+# chain = cgb.construct_graph(l)
 
+# for matrix in chain:
+#     print(matrix)
 
-def increment():
-    global counter
-    counter += 1
-    counter = counter % len(matrix_chain)
+s = torch.tensor([8.0, 8.0], requires_grad=True)
+layer = nn.Linear(2, 2)
+layer2 = nn.Linear(2, 2)
+layer3 = nn.Linear(2, 2)
 
+with torch.no_grad():
+    layer.weight = nn.Parameter(torch.tensor([[4.0, 4.0], [4.0, 4.0]]))
+    layer.bias = nn.Parameter(torch.tensor([0.0, 0.0]))
+    layer2.weight = nn.Parameter(torch.tensor([[4.0, 4.0], [4.0, 4.0]]))
+    layer2.bias = nn.Parameter(torch.tensor([0.0, 0.0]))
+    layer3.weight = nn.Parameter(torch.tensor([[4.0, 4.0], [4.0, 4.0]]))
+    layer3.bias = nn.Parameter(torch.tensor([0.0, 0.0]))
 
-def add_to_matrix_chain(
-    derivative: torch.Tensor,
-    index: int,
-):
-    derivative_row = derivative.unsqueeze(0)
-    print(index)
-    print(len(matrix_chain))
-    if len(matrix_chain) <= index:
-        matrix_chain.append(derivative_row)
-    else:
-        matrix_chain[index] = torch.cat((matrix_chain[index], derivative_row), dim=0)
+X = layer(s)
+Y = layer2(X)
+Y_0 = torch.pow(Y, 2)
+Z = layer3(Y_0)
 
+cgb = ComputationalGrapBuilder()
+chain = cgb.construct_graph(Z)
 
-seed = torch.ones_like(l[0])
-seed_vector = torch.ones_like(x)
-
-
-def traverse_graph(function, recursion_depth, curent_seed):
-    recursion_depth += 1
-    next_functions = function.next_functions
-    for _, function in enumerate(next_functions):
-        if function[0] is None:
-            continue
-        derivatives = function[0](curent_seed)
-        if isinstance(derivatives, tuple):
-            for index, derivative in enumerate(derivatives):
-                if derivative is None:
-                    continue
-                if index > 1:
-                    raise Exception("More indices than expected")
-
-                add_to_matrix_chain(derivative, recursion_depth)
-                traverse_graph(function[0], recursion_depth, curent_seed)
-
-        else:
-            add_to_matrix_chain(derivatives, recursion_depth)
-            traverse_graph(function[0], recursion_depth, curent_seed)
+for matrix in chain:
+    print(matrix)
 
 
-def construct_graph():
-    for i in range(len(l)):
-        recursion_depth = 0
-        grad_fn = l[i].grad_fn
-        derivatives = l[i].grad_fn(seed)
-        if isinstance(derivatives, tuple):
-            for index, derivative in enumerate(derivatives):
-                if derivative is None:
-                    continue
-                if index > 1:
-                    raise Exception("More indices than expected")
-
-                add_to_matrix_chain(derivative, recursion_depth)
-                traverse_graph(grad_fn, recursion_depth, curent_seed=derivative)
-        else:
-            add_to_matrix_chain(derivatives, recursion_depth)
-            traverse_graph(grad_fn, recursion_depth, curent_seed=derivatives)
-
-    print(len(matrix_chain))
-    for matrix in matrix_chain:
-        print(matrix)
+# def generate_seed(tensor):
+#     if tensor.shape[0] == 1:
+#         return torch.tensor([[1.0, 0.0]])
+#     else:
+#         return torch.tensor([1.0, 0.0]).unsqueeze(0)
 
 
-construct_graph()
+# d1 = Z[0].grad_fn(torch.tensor(1.0))
+# print(d1)
+
+# d2 = Z[0].grad_fn.next_functions[0][0](generate_seed(d1))
+# print(d2)
+
+
+# d3 = Z[0].grad_fn.next_functions[0][0].next_functions[0][0](generate_seed(d2))
+# print(d3)
+
+# d4 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0](generate_seed(d3[1]))
+# )
+# print(d4)
+
+# d5 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0](generate_seed(d4))
+# )
+# print(d5)
+
+# d6 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0](generate_seed(d5))
+# )
+# print(d6)
+
+# d7 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0](generate_seed(d6))
+# )
+# print(d7)
+
+
+# d8 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0](generate_seed(d7[1]))
+# )
+# print(d8)
+
+
+# d9 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0](generate_seed(d8))
+# )
+# print(d9)
+
+# d10 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0](generate_seed(d9))
+# )
+# print(d10)
+
+# d11 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0](generate_seed(d10))
+# )
+
+
+# d12 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0](generate_seed(d11[1]))
+# )
+
+
+# d13 = (
+#     Z[0]
+#     .grad_fn.next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions[0][0]
+#     .next_functions[0][0]
+#     .next_functions[1][0]
+#     .next_functions
+# )
+
+# print(d13)
